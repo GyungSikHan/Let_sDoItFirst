55b7ff1a47b53213007bb3bf0ea0d465a318a733
nonecalculate <escortbayonet@gmail.com>
2025-01-08 10:31:09 +0900

    Monster 클래스 추가
    확인용입니다. 잘 되는지 알려주세요!

--- TextRPG/TextRPG/TextRPG.cpp
+++ TextRPG/TextRPG/TextRPG.cpp
@@ -1,20 +1,172 @@
-﻿// TextRPG.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <string>
+#include <vector>
 
-#include <iostream>
+using namespace std;
+
+class GameManager {
+public:
+
+
+};
+
+class Item {
+public:
+    virtual string getName() = 0;
+};
+
+class Monster {
+public:
+    virtual string getName() = 0;
+    virtual int getHealth() = 0;
+    virtual int getAttack() = 0;
+    virtual void takeDamge(int damage) = 0;
+    virtual Item* dropItem();
+};
+
+
+class Goblin : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    Goblin(int level) {
+        this->health = level * 21;
+        this->attack = level * 6;
+        this->name = "고블린";
+    }
+    string getName() {
+        return name;
+    }
+    int getHealth() {
+        return health;
+    }
+    int getAttack() {
+        return attack;
+    }
+    void takeDamage(int damage) {
+        this->health -= damage;
+    }
+    Item* dropItem() {
+
+    }
+};
+
+
+class Orc : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    Orc(int level) {
+        this->health = level * 26;
+        this->attack = level * 7;
+        this->name = "오크";
+    }
+    string getName() {
+        return name;
+    }
+    int getHealth() {
+        return health;
+    }
+    int getAttack() {
+        return attack;
+    }
+    void takeDamage(int damage) {
+        this->health -= damage;
+    }
+    Item* dropItem() {
+
+    }
+};
+
+
+class Troll : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    Troll(int level) {
+        this->health = level * 28;
+        this->attack = level * 9;
+        this->name = "트롤";
+    }
+    string getName() {
+        return name;
+    }
+    int getHealth() {
+        return health;
+    }
+    int getAttack() {
+        return attack;
+    }
+    void takeDamage(int damage) {
+        this->health -= damage;
+    }
+};
+
+class Slime : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    Slime(int level) {
+        this->health = level * 24;
+        this->attack = level * 5;
+        this->name = "슬라임";
+    }
+    string getName() {
+        return name;
+    }
+    int getHealth() {
+        return health;
+    }
+    int getAttack() {
+        return attack;
+    }
+    void takeDamage(int damage) {
+        this->health -= damage;
+    }
+    Item* dropItem() {
+
+    }
+};
+
+class BossMonster : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    BossMonster(int level) {
+        this->health = level * 45;
+        this->attack = level * 15;
+        this->name = "Dragon";
+    }
+    string getName() {
+        return name;
+    }
+    int getHealth() {
+        return health;
+    }
+    int getAttack() {
+        return attack;
+    }
+    void takeDamage(int damage) {
+        this->health -= damage;
+    }
+    Item* dropItem() {
+
+    }
+};
+
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
