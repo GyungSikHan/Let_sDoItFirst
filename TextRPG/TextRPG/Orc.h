cfd610713551e21babb3c846bcb11c7689414475
Han GyungSik <gksrudtlr2@icloud.com>
2025-01-10 18:28:00 +0900

    Test Commit

---
+++ TextRPG/TextRPG/Orc.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+
+#include "Monster.h"
+
+using namespace std;
+class Orc : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    Orc(int level/*캐릭터 레벨*/) {
+        int RandomHelathBoost = rand() % 10 + 20;
+        int RandomDamageBoost = rand() % 5 + 5;
+        this->health = level * RandomHelathBoost;
+        this->attack = level * RandomDamageBoost;
+        this->name = "오크";
+    }
+    string getName() { return name; }
+    int getHealth() { return health; }
+    int getAttack() { return attack; }
+    void takeDamage(int damage) { this->health -= damage; }
+    Item* dropItem(/*아이템 관련 매개변수*/) {
+        /*아이템 종류에 따른 함수*/
+    }
+};
+
---
+++ TextRPG/TextRPG/Troll.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Monster.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+class Troll : public Monster {
+private:
+    string name;
+    int health = 0;
+    int attack = 0;
+public:
+    Troll(int level/*캐릭터 레벨*/) {
+        int RandomHelathBoost = rand() % 10 + 25;
+        int RandomDamageBoost = rand() % 5 + 7;
+        this->health = level * RandomHelathBoost;
+        this->attack = level * RandomDamageBoost;
+        this->name = "트롤";
+    }
+    string getName() { return name; }
+    int getHealth() { return health; }
+    int getAttack() { return attack; }
+    void takeDamage(int damage) { this->health -= damage; }
+    Item* dropItem(/*아이템 관련 매개변수*/) {
+        /*아이템 종류에 따른 함수*/
+    }
+};
